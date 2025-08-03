b8fa46e9a45581cb226ae7812fc22a0650a4513e
Developer RS <164728448+developer-rs5@users.noreply.github.com>
2025-08-03 14:49:36 +0530

    Update Firebase.js

--- frontend/utils/Firebase.js
+++ frontend/utils/Firebase.js
@@ -5,7 +5,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyAf8Ev21MRORLaGRm1Bt1IeiTQ729lv7Ig",
   authDomain: "e-commerce-32567.firebaseapp.com",
   projectId: "e-commerce-32567",
-  storageBucket: "e-commerce-32567.appspot.com", // Fixed the storageBucket
+  storageBucket: "e-commerce-32567.appspot.com", 
   messagingSenderId: "718486247515",
   appId: "1:718486247515:web:c5faa1d4618e78f967477e",
   measurementId: "G-K31FPGBF6S"
